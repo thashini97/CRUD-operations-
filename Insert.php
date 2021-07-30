531e72b47cb017d04fbc313ba6e2abbcd861559b
Thashini <61898159+thashini97@users.noreply.github.com>
2021-07-30 23:54:23 +0530

    insert

---
+++ Insert.php
@@ -0,0 +1,29 @@
+<?php
+ 
+ $host = 'localhost';
+ $root = 'root';
+ $password = 'password';
+ $database = 'mobile_computing';
+ 
+ 
+// Create connection
+$connect = mysqli_connect($host, $root, $password, $database);
+$Index=  $_POST['indexNo'];
+$First = $_POST['fName'];
+$Last = $_POST['lName'];
+$GPA = $_POST['gpa'];
+
+$sql = "INSERT INTO `student` (`IndexNumber`, `FirstName`, `LastName`, `GPA`)
+    values ('$Index', '$First', '$Last', '$GPA') " ;
+
+$result = mysqli_query($connect,$sql);
+
+if($result){
+    echo 'successfully added';
+
+}else{
+    echo 'failture in adding';
+}
+
+mysqli_close($connect);
+?>
