fa6e9c576d87dd35c2ce0471c9dc7fc9082d133d
Thashini <61898159+thashini97@users.noreply.github.com>
2021-07-30 23:53:34 +0530

    read

---
+++ testopen.php
@@ -0,0 +1,32 @@
+<?php
+
+$host = 'localhost';
+$root = 'root';
+$password = 'password';
+$database = 'mobile_computing';
+$connect = mysqli_connect($host, $root, $password, $database);
+		
+$ind=$_POST['indexno']; 
+  
+$stmt = "SELECT * FROM student where IndexNumber= '$ind' ";
+$result = mysqli_query($connect,$stmt);
+   
+$row = mysqli_fetch_assoc($result);
+$fname=$row['FirstName'];
+$lname=$row['LastName'];
+$gpa=$row['GPA'];
+    
+echo "Index Number  ". $ind;
+echo ("\r\n");
+echo "First Name: ". $fname;
+echo ("\r\n");   
+echo "Last Name: ". $lname;
+echo ("\r\n");
+echo "GPA: ". $gpa;  
+
+mysqli_close($connect);		
+
+
+
+?>
+
