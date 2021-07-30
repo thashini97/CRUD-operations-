04b6cd815570cf0c9471ab7402cd31e1e1d842b5
Thashini <61898159+thashini97@users.noreply.github.com>
2021-07-30 23:53:59 +0530

    update

---
+++ Update.php
@@ -0,0 +1,33 @@
+<?php
+ 
+ $host = 'localhost';
+ $root = 'root';
+ $password = 'password';
+ $database = 'mobile_computing';
+ 
+ 
+// Create connection
+$connect = mysqli_connect($host, $root, $password, $database);
+$Index=  $_POST['indexNo'];
+$First = $_POST['fName'];
+$Last = $_POST['lName'];
+$GPA = $_POST['gpa'];
+
+$stmt = $connect->prepare( "UPDATE `student` SET `IndexNumber`='$Index', `FirstName`='$First', `LastName`='$Last', `GPA`='$GPA' 
+     where IndexNumber= ? " );
+$stmt->bind_param("s", $Index);
+$stmt->execute();
+
+$result  = $stmt->get_result();;
+ 
+$stmt->close();
+if(isset($result)){
+    echo 'successfully Updated...';
+
+}else{
+    echo 'failture in updating...';
+}
+
+
+mysqli_close($connect);
+?>
